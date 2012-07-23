1d6e66e8884aab4d72a6a50912f050649ed9fe48
magdalena-kuczaba <mj.kuczaba@gmail.com>
2012-07-23 20:57:05 +0200

    warsztaty - nowe pliki statyczne

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kompany::Application.config.secret_token = '7635aab210393682e406eecb3a09aa01d6e8b31be89c9951e77b63790b89276b2dd166eadc269a61896250354153f8d5dff28b57e745d3104c4ab3fcc58c1648'
